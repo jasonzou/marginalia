b55f501dad635888b91c0bf0e594d9f83b2bbc82
geof.glass <geof.glass@18b106f2-9c29-0410-bf68-a56e300ca647>
2007-07-31 01:58:12 +0000

    Add missing files

---
+++ marginalia-php/trunk/marginalia-php/embed.php
@@ -0,0 +1,39 @@
+<?php
+
+function listMarginaliaJavascript( )
+{
+	return array (
+		"3rd-party.js"
+		,"3rd-party/cssQuery.js"
+		,"3rd-party/cssQuery-level2.js"
+		,"3rd-party/cssQuery-standard.js"
+	
+		,"log.js"
+		,"prefs.js"
+		,"html-model.js"
+		,"domutil.js"
+		,"ranges.js"
+		,"SequenceRange.js"
+		,"XPathRange.js"
+		,"annotation.js"
+		,"post-micro.js"
+		,"linkable.js"
+		,"link-ui-clicktolink.js"
+		
+		,"marginalia.js"
+		,"blockmarker-ui.js"
+		,"highlight-ui.js"
+		,"note-ui.js"
+		,"link-ui.js"
+		,"link-ui-simple.js"
+	
+		,"smartcopy.js"
+		,"RangeInfo.js"
+		,"rest-annotate.js"
+		,"rest-prefs.js"
+		,"rest-keywords.js"
+		,"marginalia-direct.js"
+	);
+}
+	
+?>
---
+++ marginalia-php/trunk/marginalia-php/marginalia-constants.php
@@ -0,0 +1,14 @@
+<?
+
+// Post Micro format:
+define( 'PM_POST_CLASS', 'hentry' );				// this is an addressable fragment for annotation
+define( 'PM_CONTENT_CLASS', 'entry-content' );	// the content portion of a fragment
+define( 'PM_TITLE_CLASS', 'entry-title' );		// the title of an annotated fragment
+define( 'PM_AUTHOR_CLASS', 'author' );				// the author of the fragment
+define( 'PM_DATE_CLASS', 'published' );			// the creation/modification date of the fragment
+define( 'PM_URL_REL', 'bookmark' );				// the url of this fragment (uses rel rather than class attribute)
+
+// Annotation format:
+define( 'AN_NOTES_CLASS', 'notes' );			// the notes portion of a fragment
+
+?>
