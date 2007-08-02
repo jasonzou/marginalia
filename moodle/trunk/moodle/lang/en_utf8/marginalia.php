1ad63e1383c5ea19e311452113f6739331ad8f91
geof.glass <geof.glass@18b106f2-9c29-0410-bf68-a56e300ca647>
2007-08-02 04:17:01 +0000

    Add localized strings

---
+++ moodle/trunk/moodle/lang/en_utf8/marginalia.php
@@ -0,0 +1,43 @@
+<?php // $Id$
+
+$string['summary_title'] = 'Annotation Summary';
+$string['prompt_find'] = 'Find';
+$string['prompt_by'] = 'by';
+$string['search_of_all'] = 'all annotations';
+$string['search_of_self'] = 'annotations of my work';
+$string['search_by_all'] = 'anyone';
+$string['search_by_self'] = 'myself';
+$string['search_by_teachers'] = 'teachers';
+$string['search_by_students'] = 'students';
+$string['search_text'] = 'containing';
+$string['summary_range_error'] =
+	'You have been directed here because one or more annotations could not'
+	. ' be displayed.  This is probably because the annotated text changed.'
+	. ' This summary includes the annotations which could not be shown.';
+$string['prompt_search_desc'] = 'Show';
+$string['prompt_section'] = 'Go to $a->section_type';
+$string['prompt_row'] = 'Go to $a->row_type by $a->author';
+$string['private'] = 'private';
+$string['public'] = 'public';
+$string['author'] = 'author';
+$string['teacher'] = 'teacher';
+$string['author+teacher'] = 'both';
+$string['atom_feed'] = 'Atom 1.0 Feed';
+$string['atom_feed_desc'] = 'Subscribe to updates to this page (only recent public annotations will be included).';
+$string['unknown_course'] = 'unknown course';
+$string['all_discussions'] = 'all discussions';
+$string['discussion_name'] = 'discussion \\"$a->name\\"';
+$string['unknown_discussion'] = 'unknown discussion';
+$string['whole_course'] = 'whole course';
+$string['unknown_post'] = 'unknown discussion post';
+$string['post_name'] = 'discussion post "$a->name"';
+
+$string['annotation_desc_authorsearch'] = 'annotations by $a->who of work by $a->by containing \\"$a->search\\" in $a->title';
+$string['annotation_desc_author'] = 'annotations by $a->who of work by $a->by in $a->title';
+$string['annotation_desc_search'] = 'annotations by $a->who containing \\"$a->search\\" in $a->title';
+$string['annotation_desc'] = 'annotations by $a->who in $a->title';
+$string['tip'] = 'Tip';
+$string['smartcopy_help'] = 'The Smartcopy feature automatically includes context information'
+	. ' when you copy and paste text from a blog post.  To switch it on or off, press'
+	. ' Shift-Ctrl-S while viewing a discussion forum.';
+?>
