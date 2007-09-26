eccf88e6edd6a780fce1aa5649af684416cff5f2
geof.glass <geof.glass@18b106f2-9c29-0410-bf68-a56e300ca647>
2007-09-26 20:44:25 +0000

    Change require to require_once

--- ojs/trunk/MarginaliaPlugin/index.php
+++ ojs/trunk/MarginaliaPlugin/index.php
@@ -16,17 +16,17 @@ error_reporting( E_ALL );
 
 define( 'DEBUG_ANNOTATION_QUERY', false );	// will break GET operations by emitting query string
 
-require( 'marginalia-php/MarginaliaHelper.php' );
-require( 'marginalia-php/SequenceRange.php' );
-require( 'marginalia-php/XPathRange.php' );
-require( 'MarginaliaPlugin.inc.php' );
-require( 'Annotation.inc.php' );
-require( 'AnnotationDAO.inc.php' );
-require( 'Keyword.inc.php' );
-require( 'KeywordDAO.inc.php' );
-require( 'Preference.inc.php' );
-require( 'PreferenceDAO.inc.php' );
-require( 'MarginaliaStrings.inc.php' );
+require_once( 'marginalia-php/MarginaliaHelper.php' );
+require_once( 'marginalia-php/SequenceRange.php' );
+require_once( 'marginalia-php/XPathRange.php' );
+require_once( 'MarginaliaPlugin.inc.php' );
+require_once( 'Annotation.inc.php' );
+require_once( 'AnnotationDAO.inc.php' );
+require_once( 'Keyword.inc.php' );
+require_once( 'KeywordDAO.inc.php' );
+require_once( 'Preference.inc.php' );
+require_once( 'PreferenceDAO.inc.php' );
+require_once( 'MarginaliaStrings.inc.php' );
 return new MarginaliaPlugin();
 
 ?> 
