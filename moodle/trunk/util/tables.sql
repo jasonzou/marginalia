853bb029f3e43b6bb346cf9e8fe4c94e8a849ba2
geof.glass <geof.glass@18b106f2-9c29-0410-bf68-a56e300ca647>
2008-12-14 06:19:53 +0000

    Update help file Remove stuff in moodle/user, which was needed when username changed - no longer necessary as userid is used internally instead

--- moodle/trunk/util/tables.sql
+++ moodle/trunk/util/tables.sql
@@ -5,28 +5,24 @@ create table mdl_annotation (
 	url varchar(255) not null,
 	start_block varchar(255),
 	start_xpath varchar(255),
+	start_line int,
 	start_word int,
 	start_char int,
 	end_block varchar(255),
 	end_xpath varchar(255),
+	end_line int,
 	end_word int,
 	end_char int,
 	note varchar(255) null,
-	created datetime not null,	
-	modified timestamp not null,
 	quote text null,
 	quote_title varchar(255) null,
 	quote_author varchar(255) null,
 	action varchar(30) null,
 	link varchar(255) null,
 	link_title varchar(255) null,
-	version int null,
+	created datetime not null,	
+	modified timestamp not null,
 	object_type varchar(16) null,
 	object_id int null
 );
 
-create table mdl_annotation_keywords (
-	userid bigint(10) unsigned not null,
-	name varchar(255) not null,
-	description text null
-);
